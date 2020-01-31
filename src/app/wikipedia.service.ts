90a37b5f8d05806b86762c24e23b2ba674aed0a8
Ruben80 <43403734+Ruben80@users.noreply.github.com>
2020-01-31 16:01:22 +0100

    Add files via upload

---
+++ src/app/wikipedia.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string;
+      snippet: string;
+      pageid: number;
+    }[]
+  }
+}
+
+// interface Car { 
+//   year: number;
+//   color: string;
+//   running: boolean;
+//   make: { 
+//     name: string;
+//     dateCreated: number
+//   }
+// }
+
+// const observable = new Observable<Car>((observer) => {
+//   observer.next(1)({
+//     year: 2000,
+//     color: 'red',
+//     running: true,
+//     make: {
+//       name: 'Chevy',
+//       dateCreated: 1950
+//     }
+//   });
+// }).pipe(
+//   pluck('make', 'dateCreated' )
+
+// );
+
+// observable.subscribe((value) => {
+//   console.log(value);
+// });
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WikipediaService {
+  constructor(private http: HttpClient) { }
+
+  public search(term: string) {
+    return this.http
+      .get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          format: 'json',
+          list: 'search',
+          utf8: '1',
+          srsearch: term,
+          origin: '*'
+        }
+      }).pipe(
+        pluck('query', 'search')
+      );
+  }
+}
